1671f6b94bd49aaf6a44c7ac7ac8cbcbda502866
Matt Layman <matthewlayman@gmail.com>
2016-06-11 16:13:44 -0400

    Actually test the plugin.
    Fixes #18

--- pytest_tap/tests/test_plugin.py
+++ pytest_tap/tests/test_plugin.py
@@ -6,9 +6,10 @@ except ImportError:
     import mock
 import tempfile
 
-from tap.plugins import _pytest
 from tap.tests import TestCase
 from tap.tracker import Tracker
+
+from pytest_tap import plugin
 
 
 class TestPlugin(TestCase):
@@ -17,11 +18,11 @@ class TestPlugin(TestCase):
         """The pytest plugin uses module scope so a fresh tracker
         must be installed each time."""
         # When running this suite with pytest, save and restore the tracker.
-        self._tracker = _pytest.tracker
-        _pytest.tracker = Tracker()
+        self._tracker = plugin.tracker
+        plugin.tracker = Tracker()
 
     def tearDown(self):
-        _pytest.tracker = self._tracker
+        plugin.tracker = self._tracker
 
     def _make_config(self):
         config = mock.Mock()
@@ -35,85 +36,85 @@ class TestPlugin(TestCase):
         group = mock.Mock()
         parser = mock.Mock()
         parser.getgroup.return_value = group
-        _pytest.pytest_addoption(parser)
+        plugin.pytest_addoption(parser)
         self.assertEqual(group.addoption.call_count, 4)
 
     def test_tracker_stream_set(self):
         config = self._make_config()
         config.option.tap_stream = True
-        _pytest.pytest_configure(config)
-        self.assertTrue(_pytest.tracker.streaming)
+        plugin.pytest_configure(config)
+        self.assertTrue(plugin.tracker.streaming)
 
     def test_tracker_outdir_set(self):
         outdir = tempfile.mkdtemp()
         config = self._make_config()
         config.option.tap_outdir = outdir
-        _pytest.pytest_configure(config)
-        self.assertEqual(_pytest.tracker.outdir, outdir)
+        plugin.pytest_configure(config)
+        self.assertEqual(plugin.tracker.outdir, outdir)
 
     def test_tracker_combined_set(self):
         config = self._make_config()
         config.option.tap_combined = True
-        _pytest.pytest_configure(config)
-        self.assertTrue(_pytest.tracker.combined)
+        plugin.pytest_configure(config)
+        self.assertTrue(plugin.tracker.combined)
 
     def test_track_when_call_report(self):
         """Only the call reports are tracked."""
-        _pytest.tracker = mock.Mock()
+        plugin.tracker = mock.Mock()
         report = mock.Mock(when='setup', outcome='passed')
-        _pytest.pytest_runtest_logreport(report)
-        self.assertFalse(_pytest.tracker.add_ok.called)
+        plugin.pytest_runtest_logreport(report)
+        self.assertFalse(plugin.tracker.add_ok.called)
 
     def test_tracks_ok(self):
-        _pytest.tracker = mock.Mock()
+        plugin.tracker = mock.Mock()
         location = ('test_file.py', 1, 'TestFake.test_me')
         report = mock.Mock(when='call', outcome='passed', location=location)
-        _pytest.pytest_runtest_logreport(report)
-        _pytest.tracker.add_ok.assert_called_once_with(
+        plugin.pytest_runtest_logreport(report)
+        plugin.tracker.add_ok.assert_called_once_with(
             'TestFake', 'TestFake.test_me')
 
     def test_tracks_not_ok(self):
-        _pytest.tracker = mock.Mock()
+        plugin.tracker = mock.Mock()
         location = ('test_file.py', 1, 'TestFake.test_me')
         report = mock.Mock(when='call', outcome='failed', location=location)
-        _pytest.pytest_runtest_logreport(report)
-        _pytest.tracker.add_not_ok.assert_called_once_with(
+        plugin.pytest_runtest_logreport(report)
+        plugin.tracker.add_not_ok.assert_called_once_with(
             'TestFake', 'TestFake.test_me', diagnostics='')
 
     def test_tracks_skip(self):
-        _pytest.tracker = mock.Mock()
+        plugin.tracker = mock.Mock()
         location = ('test_file.py', 1, 'TestFake.test_me')
         longrepr = ('', '', 'Skipped: a reason')
         report = mock.Mock(
             when='call', outcome='skipped', location=location,
             longrepr=longrepr)
-        _pytest.pytest_runtest_logreport(report)
-        _pytest.tracker.add_skip.assert_called_once_with(
+        plugin.pytest_runtest_logreport(report)
+        plugin.tracker.add_skip.assert_called_once_with(
             'TestFake', 'TestFake.test_me', 'a reason')
 
     def test_generates_reports_for_stream(self):
         config = self._make_config()
         config.option.tap_stream = True
-        _pytest.tracker = mock.Mock()
-        _pytest.pytest_unconfigure(config)
-        _pytest.tracker.generate_tap_reports.assert_called_once_with()
+        plugin.tracker = mock.Mock()
+        plugin.pytest_unconfigure(config)
+        plugin.tracker.generate_tap_reports.assert_called_once_with()
 
     def test_generates_reports_for_files(self):
         config = self._make_config()
         config.option.tap_files = True
-        _pytest.tracker = mock.Mock()
-        _pytest.pytest_unconfigure(config)
-        _pytest.tracker.generate_tap_reports.assert_called_once_with()
+        plugin.tracker = mock.Mock()
+        plugin.pytest_unconfigure(config)
+        plugin.tracker.generate_tap_reports.assert_called_once_with()
 
     def test_generates_reports_for_combined(self):
         config = self._make_config()
         config.option.tap_combined = True
-        _pytest.tracker = mock.Mock()
-        _pytest.pytest_unconfigure(config)
-        _pytest.tracker.generate_tap_reports.assert_called_once_with()
+        plugin.tracker = mock.Mock()
+        plugin.pytest_unconfigure(config)
+        plugin.tracker.generate_tap_reports.assert_called_once_with()
 
     def test_skips_reporting_with_no_output_option(self):
         config = self._make_config()
-        _pytest.tracker = mock.Mock()
-        _pytest.pytest_unconfigure(config)
-        self.assertFalse(_pytest.tracker.generate_tap_reports.called)
+        plugin.tracker = mock.Mock()
+        plugin.pytest_unconfigure(config)
+        self.assertFalse(plugin.tracker.generate_tap_reports.called)
