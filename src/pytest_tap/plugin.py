578c0e59f4fa63da65893028f2c5b921366c6173
Matt Layman <matthewlayman@gmail.com>
2019-10-19 23:56:29 -0400

    Track setup failures. (#44)
    * Track setup failures.
    
    Fixes #39
    
    * Update release notes.

--- src/pytest_tap/plugin.py
+++ src/pytest_tap/plugin.py
@@ -90,10 +90,12 @@ def pytest_runtest_logreport(report):
     if not ENABLED:
         return
 
-    if not (
+    is_trackable_result = (
         (report.when == "setup" and report.outcome == "skipped")
+        or (report.when == "setup" and report.outcome == "failed")
         or report.when == "call"
-    ):
+    )
+    if not is_trackable_result:
         return
 
     description = str(report.location[0]) + "::" + str(report.location[2])
