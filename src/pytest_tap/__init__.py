54356c3a7a1ce2f4616dda3319d017663c4c81dc
Matt Layman <matthewlayman@gmail.com>
2020-01-28 11:18:55 -0500

    Update copyright year.

--- src/pytest_tap/__init__.py
+++ src/pytest_tap/__init__.py
@@ -1,3 +1,3 @@
-# Copyright (c) 2019, Matt Layman
+# Copyright (c) 2020, Matt Layman
 
 __version__ = "2.5"
--- transifex.py
+++ transifex.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, Matt Layman
+# Copyright (c) 2020, Matt Layman
 
 from ConfigParser import ConfigParser, NoOptionError, NoSectionError
 import os
